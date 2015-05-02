b4b38331c56815f7a06b42bb2a6a791855166713
Barry <barry@io.co.za>
2015-05-02 19:11:42 +0200

    added provisioning of libssl1.0.0

--- provision.sh
+++ provision.sh
@@ -1,6 +1,6 @@
 # updated system pls
-#sudo apt-get update -y
-#sudo apt-get upgrade -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
 # install basics used for usb provisioning
 sudo apt-get install mbr syslinux mtools p7zip-full grub-efi-amd64-bin git wget build-essential reprepro -y
@@ -37,6 +37,9 @@ sudo reprepro -Vb /vagrant/reprepro/. includedeb goddard /var/cache/apt/archives
 sudo reprepro -Vb /vagrant/reprepro/. includedeb goddard /var/cache/apt/archives/zlib*
 sudo reprepro -Vb /vagrant/reprepro/. includedeb goddard /var/cache/apt/archives/lxc*
 
+# and libssl1.0.0 because of versioning conflicts
+apt-get download libssl1.0.0
+sudo reprepro -Vb /vagrant/reprepro/. includedeb goddard ./libssl1.0.0*
 
 # add no strict host key checking when cloning from github
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
